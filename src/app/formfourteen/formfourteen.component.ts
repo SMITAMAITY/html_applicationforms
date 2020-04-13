e871db279283739ff7876b7e473f9b5aa17af7c6
SMITAMAITY <smitam996@gmail.com>
2020-04-13 12:42:22 +0530

    Third Commit

--- src/app/formfourteen/formfourteen.component.ts
+++ src/app/formfourteen/formfourteen.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'
+import { Time } from '@angular/common';
 
 @Component({
   selector: 'app-formfourteen',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormfourteenComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  url:string= "http://localhost:8080/OfferRelease";
+  userForm: FormGroup;
+  mailId: string;
+  Name:string;
+  cv:any;
+  dob:Date;
+  contact:number;
+  registrationDate:Date;
+  approver:string;
+  confirmation:string;
+  reason:string;
+  decision:string;
+  result:any;
+  
+  constructor(private http:HttpClient,private router: Router, private formBuilder: FormBuilder) { }
+  
+  ngOnInit() {
+   this.userForm = this.formBuilder.group({
+    mailId: [],
+    Name:[],
+    cv:[],
+    dob:[],
+    contact:[],
+    registrationDate:[],
+    approver:[],
+    confirmation:[],
+    reason:[],
+    decision:[]
+   });
+  }
+  
+  submit():void{
+    // alert('login');
+   // let url = this.url+"/"+this.Username+"/"+this.password;
+  //  alert(this.url);
+   this.http.post(this.url,this.userForm.value).subscribe(data=>{
+    //alert('Login is successful');
+    
+    this.result = data;
+  
+    this.router.navigate(['fifteen']);
+   });
+  
   }
 
-}
+ 
+  }
